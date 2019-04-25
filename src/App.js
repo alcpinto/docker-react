869242fbe7536037fca4b2f70bdc799af93c6ebf
alcpinto <abilio@fraxses.com>
2019-04-25 14:10:03 +0100

    Changed app text

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React Fake!...
+          I was changed on Feature Branch!
         </a>
       </header>
     </div>
